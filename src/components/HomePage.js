94f0ea3f17c6d365428f7281f15245261c7645b9
Grey Elerson <grey.l.elerson@gmail.com>
2024-04-05 23:27:05 -0600

    Add firestore

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { db } from '../config/firestore';
+import { collection, addDoc } from 'firebase/firestore';
 
 function HomePage() {
+  // const addData = async () => {
+  //   try {
+  //     const docRef = await addDoc(collection(db, 'users'), {
+  //       name: 'Ada Lovelace',
+  //       email: 'nobody@nowhere.no',
+  //     });
+  //     console.log('Document written with ID: ', docRef.id);
+  //   }
+  //   catch (e) {
+  //     console.error('Error adding document: ', e);
+  //   }
+  // }
+
+  // addData();
   return (
     <div>
       <h1>Home Page</h1>
