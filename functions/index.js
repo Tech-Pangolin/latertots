664cb6e3d36b2ee9a8f5d18dd3d468ed9f738ea5
Grey Elerson <grey.l.elerson@gmail.com>
2024-05-08 19:06:20 -0600

    feat(cloud fxns): Add validateNewUser function to handle new user creation from auth

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,37 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const functions = require('firebase-functions');
 const logger = require("firebase-functions/logger");
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.validateNewUser = functions.firestore
+    .document('Users/{docId}')
+    .onCreate((snap, context) => {
+        // Get the current data
+        const receivedData = snap.data();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+        // Populate the standard data shape
+        const newValidUser = {
+            CellNumber: "",
+            City: "",
+            Email: receivedData.email,
+            Name: receivedData.displayName || "",
+            Role: db.collection('Roles').doc('parent-user'),
+            State: "",
+            StreetAddress: "",
+            Zip: ""
+        };
+
+        // Save the updated document
+        return snap.ref.set(newValidUser, { merge: false }).then(() => {
+          logger.info(`Success: Document (Users/${context.params.docId}) updated`);
+        }).catch((error) => {
+          logger.error(`Error updating document (Users/${context.params.docId}):`, error);
+        }); 
+    });
+
+// Deployment command: firebase deploy --only functions
+// Run from the root of the project directory
