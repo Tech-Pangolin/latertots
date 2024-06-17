ec1fb5ba57b09ed23df6e7cd59609e95fd2dcaeb
Grey Elerson <grey.l.elerson@gmail.com>
2024-06-17 16:43:15 -0600

    fix(profile): Display user's profile photo on profile

--- src/components/Pages/UserProfile.js
+++ src/components/Pages/UserProfile.js
@@ -43,7 +43,7 @@ const UserProfile = () => {
       <div className="row">
         <div className="col-md-3 border-right">
           <div className="d-flex flex-column align-items-center text-center p-3 py-5">
-            <img className="rounded-circle mt-5" width="150px" src={currentUser.photo ? currentUser.photo :"https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"} />
+            <img className="rounded-circle mt-5" width="150px" src={user.PhotoURL ? user.PhotoURL :"https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"} />
             <span className="font-weight-bold">{currentUser?.displayName}</span><span className="text-black-50">{currentUser.email}</span><span> </span>
             </div>
         </div>
