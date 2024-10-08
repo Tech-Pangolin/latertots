11d6281d7b39e2a53b50e319d259fbf311f73500
Terra Taylor <terra.taylor@gmail.com>
2024-10-08 13:55:56 -0500

    image path correction

--- src/components/Pages/IntroPage.js
+++ src/components/Pages/IntroPage.js
@@ -11,46 +11,6 @@ function IntroPage() {
 
     return (
         <>
-            {/* <header id="header" className="fixed-top d-flex align-items-center header-transparent">
-    <div className="container d-flex align-items-center justify-content-between">
-
-      <div className="logo">
-        <h1><a href="index.html"><span>Bootslander</span></a></h1>
-        <a href="index.html"><img src="assets/img/logo.png" alt="" className="img-fluid"></a>
-      </div>
-
-      <nav id="navbar" className="navbar">
-        <ul>
-          <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a className="nav-link scrollto" href="#about">About</a></li>
-          <li><a className="nav-link scrollto" href="#features">Features</a></li>
-          <li><a className="nav-link scrollto" href="#gallery">Gallery</a></li>
-          <li><a className="nav-link scrollto" href="#team">Team</a></li>
-          <li><a className="nav-link scrollto" href="#pricing">Pricing</a></li>
-          <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
-        <i className="bi bi-list mobile-nav-toggle"></i>
-      </nav>
-
-    </div>
-  </header> */}
             {/* HERO SECTION */}
             <section id="hero">
                 <div className="container-fluid">
@@ -66,7 +26,7 @@ function IntroPage() {
                             </div>
                         </div>
                         <div className="col-sm-12 col-lg-4 order-2 order-lg-1 order-sm-1 hero-img" dataAos="zoom-out" dataAosDelay="300">
-                            <img src="assets/img/brand/Logos/png/primaryLogos/primaryWhite.png" className="img-fluid animated slide-in" style={{width:"75%"}}  alt="" />
+                            <img src="assets/img/brand/Logos/PNG/primaryLogos/primaryWhite.png" className="img-fluid animated slide-in" style={{width:"75%"}}  alt="" />
                         </div>
                     </div>
                 </div>
