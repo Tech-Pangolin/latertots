0978a68dcfa8fe03dd9ce4ee2f91d7178a42fb5d
Grey Elerson <grey.l.elerson@gmail.com>
2024-11-29 14:36:44 -0700

    fix: update Auth workflow to initialize Users collection doc on registration

--- src/Helpers/firebase.js
+++ src/Helpers/firebase.js
@@ -1,8 +1,8 @@
-import { collection, getDocs, getDoc, where, query, arrayUnion, updateDoc, addDoc, setDoc, doc, deleteDoc, Timestamp } from "@firebase/firestore";
+import { collection, getDocs, getDoc, where, query, arrayUnion, updateDoc, addDoc, doc, setDoc, deleteDoc, Timestamp } from "@firebase/firestore";
 import { db } from "../config/firestore";
 import { ref, uploadBytes, getDownloadURL } from "@firebase/storage";
 import { storage } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, deleteUser } from "firebase/auth";
 import { logger } from "./logger";
 
 
@@ -20,6 +20,37 @@ export class FirebaseDbService {
   async fetchUserRole(roleRef) {
     const roleSnapshot = await getDoc(roleRef);
     return roleSnapshot.exists() ? roleSnapshot.id : null;
+  }
+
+  /**
+   * Fetches the role of a user based on the provided user ID.
+   * 
+   * @param {string} userId - The ID of the user to fetch the role for.
+   * @returns {Promise<string|null>} - A promise that resolves to the role ID if found, or null if not found.
+   */
+  async fetchRoleByUserId(userId) {
+    const userDocRef = doc(db, 'Users', userId);
+    const userDoc = await getDoc(userDocRef);
+    if (userDoc.exists()) {
+      const roleRef = userDoc.data().Role;
+      return await this.fetchUserRole(roleRef);
+    }
+    return null;
+  }
+
+  /**
+   * Fetches the avatar photo URL of a user based on the provided user ID.
+   * 
+   * @param {string} userId - The ID of the user to fetch the avatar photo for.
+   * @returns {Promise<string|null>} - A promise that resolves to the avatar photo URL if found, or null if not found.
+   */
+  async fetchAvatarPhotoByUserId(userId) {
+    const userDocRef = doc(db, 'Users', userId);
+    const userDoc = await getDoc(userDocRef);
+    if (userDoc.exists()) {
+      return userDoc.data().PhotoURL || null;
+    }
+    return null;
   }
 
   validateAuth(requiredRole = null) {
@@ -490,50 +521,38 @@ export class FirebaseDbService {
    * @returns {Promise<User>} A promise that resolves to the authenticated user object.
    */
   createUserAndAuthenticate = async (firebaseAuth, email, password) => {
+    let userCredential = null
     try {
-      const userCredential = await createUserWithEmailAndPassword(firebaseAuth, email, password);
-      logger.info('User created and authenticated:', userCredential.user);
-
-      const roleRef = doc(db, 'Roles', 'parent-user');
+      userCredential = await createUserWithEmailAndPassword(firebaseAuth, email, password);
+      const uid = userCredential.user.uid;
 
       try {
-        const attempt = 0;
-        while (attempt < 10 && userCredential.user) {
-          const userDocRef = doc(db, 'Users', userCredential.user.uid);
-          const userDoc = await getDoc(userDocRef);
-          if (userDoc.exists()) {
-            logger.info('User document found:', userDoc.data());
-            break;
-          }
-          logger.info('User document not found, retrying... (attempt', attempt + 1, '/10)');
-          await new Promise(r => setTimeout(r, 1000 * attempt));
-        }
-
-        await setDoc(doc(db, 'Users', userCredential.user.uid), 
-        { 
+        await setDoc(doc(db, 'Users', uid), {
           CellNumber: "",
           City: "",
           Email: userCredential.user.email,
-          Name: userCredential.user.displayName || "",
+          Name:   userCredential.user.displayName || "",
+          Role: doc(db, 'Roles', 'parent-user'),
           State: "",
           StreetAddress: "",
           Zip: "",
-          archived: false,
-          Role: roleRef,
-        }, { merge: true });
-      
-        logger.info('User document created:', userCredential.user.uid);
+          archived: false
+      });
+        logger.info("User document initialized with template data.");
       } catch (error) {
-        logger.error('createUserAndAuthenticate: Error creating user document:', error);
+        logger.error("createUserAndAuthenticate: Could not initialize /Users document with template data: ", error);
         throw error;
       }
-      logger.info('createUserAndAuthenticate: Returning userCredential.user', userCredential.user);
+
+      logger.info("User created and authenticated:", userCredential.user);
       return userCredential.user;
     } catch (error) {
-      logger.error('createUserAndAuthenticate: Error:', error);
+      if (userCredential) {
+        await deleteUser(userCredential.user);
+      }
+      logger.error("createUserAndAuthenticate: Could not create new FirebaseAuth account: ", error);
       throw error;
     }
-    
   };
 
 
--- src/components/AuthProvider.js
+++ src/components/AuthProvider.js
@@ -1,17 +1,14 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
 import { app } from '../config/firebase';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../config/firestore';
 import { logger } from '../Helpers/logger';
+import { FirebaseDbService } from '../Helpers/firebase';
 
 const auth = getAuth();
 
 export const signInWithGoogle = async () => {
   try {
     await signInWithPopup(auth, new GoogleAuthProvider());
-    // Redirect to "/" if sign-in is successful
-    window.location.href = "/profile";
   } catch (error) {
     console.error("Error signing in with Google: ", error.message);
   }
@@ -20,7 +17,6 @@ export const signInWithGoogle = async () => {
 export const signInWithEmail = async (email, password) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
-    window.location.href = "/profile";
   } catch (error) {
     console.error("Error signing in with email and password: ", error.message);
   }
@@ -32,72 +28,43 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [dbService, setDbService] = useState(null);
   const auth = getAuth(app);
 
   useEffect(() => {
-    // Listen for auth state changes
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        try {
-          // Force token refresh to ensure latest claims
-          const idTokenResult = await user.getIdTokenResult(true);
-          const claims = idTokenResult.claims;
-          logger.info('AuthProvider: fetched latest claims')
+      setDbService(new FirebaseDbService(user)); // Cannot use this immediately after setting dbService
+      const tempDbService = new FirebaseDbService(user);
 
-          // Fetch user data from Firestore
-          const fetchWithRetry = async (maxRetries = 7, delay = 750) => {
-            let attempts = 0;
-            while (attempts < maxRetries) {
-              try {
-                logger.info(`AuthProvider: Attempting to fetch user data (attempt ${attempts + 1})`);
-                const userDocRef = await getDoc(doc(db, 'Users', user.uid));
-                const userDocData = userDocRef.data();
+      const userRole = await tempDbService.fetchRoleByUserId(user.uid)
+      const userPhoto = await tempDbService.fetchAvatarPhotoByUserId(user.uid)
 
-                return userDocData;
-              } catch (error) {
-                attempts++;
-                logger.warn(`AuthProvider: Attempt ${attempts} failed:`, error);
-
-                if (attempts >= maxRetries) {
-                  throw new Error(`Failed to fetch user data after maximum ${attempts} attempts`);
-                }
-
-                await new Promise((resolve) => setTimeout(resolve, delay));
-              }
-            }
-          }
-
-          const userDocData = await fetchWithRetry();
-
-          // Include custom claims in the user object
-          setCurrentUser({
-            ...user,
-            role: claims.role || 'parent-user', // Default to parent user if no role is set
-            photoURL: userDocData.PhotoURL || null,
-          });
-        } catch (error) {
-          logger.error("Error getting custom claims: ", error);
-        }
-      } else {
-        setCurrentUser(user);
+      const loggedInUser = {
+        ...user,
+        role: userRole,
+        photoURL: userPhoto
       }
+
+      setCurrentUser(loggedInUser);
       setLoading(false);
     });
 
-    // Cleanup subscription
-    return unsubscribe;
+    return () => {
+      unsubscribe();
+    };
   }, [auth]);
 
   const logout = async () => {
     try {
       await auth.signOut();
+      setDbService(null);
     } catch (error) {
       logger.error("Error logging out: ", error);
     }
   };
 
   return (
-    <AuthContext.Provider value={{ currentUser, logout }}>
+    <AuthContext.Provider value={{ currentUser, logout, dbService }}>
       {!loading && children}
     </AuthContext.Provider>
   );
