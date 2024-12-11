0832e11b6538453592c34bb50342e4f3b82d7aae
Terra Taylor <terra.taylor@gmail.com>
2024-12-11 09:33:30 -0600

    navbar and site updates

--- src/components/Pages/TotsTidbits.js
+++ src/components/Pages/TotsTidbits.js
@@ -8,7 +8,7 @@ function TotsTidbits() {
             <section id="tidbits" className="tidbits-hero" >
                 <div className="container">
                     <div className="row justify-content-center">
-                        <div className="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
+                        <div className="col-lg-12 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
 
                         </div>
 
