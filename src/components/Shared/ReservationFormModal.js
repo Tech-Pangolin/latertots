2f6a7bd98c7f1dd7876ac1c475ff09c96d9ed79b
Grey Elerson <grey.l.elerson@gmail.com>
2025-01-15 16:08:22 -0700

    fix: formatting on multiselect field

--- src/components/Shared/ReservationFormModal.js
+++ src/components/Shared/ReservationFormModal.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Button, TextField, Dialog, DialogActions, DialogTitle, DialogContent, MenuItem } from '@mui/material';
+import { Button, TextField, Dialog, DialogActions, DialogTitle, DialogContent, MenuItem, Select, InputLabel, OutlinedInput, FormControl } from '@mui/material';
 import { checkAgainstBusinessHours, checkFutureStartTime, getCurrentDate, getCurrentTime } from '../../Helpers/calendar';
 import { v4 as uuidv4 } from 'uuid';
 import { FirebaseDbService } from '../../Helpers/firebase';
 import { useAuth } from '../AuthProvider';
+import { logger } from '../../Helpers/logger';
 
 
 // Remember to create state for the open/closed state of the modal and the form data
-const ReservationFormModal = ({modalOpenState = false, setModalOpenState, children, setEvents, events, handleScheduleSave, currentUserData}) => {
+const ReservationFormModal = ({ modalOpenState = false, setModalOpenState, children, setEvents, events, handleScheduleSave, currentUserData }) => {
   const { currentUser } = useAuth();
   const [dbService, setDbService] = useState(null);
 
@@ -16,7 +17,7 @@ const ReservationFormModal = ({modalOpenState = false, setModalOpenState, childr
   }, [currentUser]);
 
   const initialState = {
-    selectedChild: {id: '', name: 'Select a child'},
+    selectedChild: [],
     date: `${getCurrentDate()}`,
     start: `${getCurrentTime()}`,
     end: `${getCurrentTime()}`
@@ -29,19 +30,14 @@ const ReservationFormModal = ({modalOpenState = false, setModalOpenState, childr
   };
 
   const childrenOptions = [
-    {id: '', name: "Select a child"},
      ...children.map( child => Object.fromEntries([["id", child.id], ["name", child.Name]]) )
   ];
 
   const handleChange = (e) => {
-    if (e.target.name === 'selectedChild') {
-      setFormData({ ...formData, 'selectedChild': childrenOptions.find(child => child.id === e.target.value)});
-    } else {
-      setFormData({ ...formData, [e.target.name]: e.target.value });
-    }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  useEffect(() => { 
+  useEffect(() => {
     const hasNewEvent = events.some(event => event.id.includes('-') && event.extendedProps.fromForm)
 
     if (hasNewEvent) {
@@ -52,13 +48,13 @@ const ReservationFormModal = ({modalOpenState = false, setModalOpenState, childr
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newEvent = {
-      id: uuidv4(), 
+      id: uuidv4(),
       title: formData.selectedChild.name,
       start: new Date(formData.date + 'T' + formData.start).toISOString(),
-      end:  new Date(formData.date + 'T' + formData.end).toISOString(),
+      end: new Date(formData.date + 'T' + formData.end).toISOString(),
       allDay: false,
       extendedProps: {
-        duration: (new Date(formData.date + 'T' + formData.end).toISOString() - new Date(formData.date + 'T' + formData.start).toISOString())/3600_000,
+        duration: (new Date(formData.date + 'T' + formData.end).toISOString() - new Date(formData.date + 'T' + formData.start).toISOString()) / 3600_000,
         status: 'pending',
         childId: formData.selectedChild.id,
         fromForm: true
@@ -78,63 +74,77 @@ const ReservationFormModal = ({modalOpenState = false, setModalOpenState, childr
   };
 
   return (
-    <Dialog open={modalOpenState}  aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Reservation</DialogTitle>
-        <DialogContent>
-          <form onSubmit={handleSubmit}>
-            <TextField
+    <Dialog open={modalOpenState} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Reservation</DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleSubmit}>
+
+          <FormControl fullWidth style={{ marginTop: '1rem' }}>
+            <InputLabel id="multiselect-child-label">Name</InputLabel>
+            <Select
+              labelId="multiselect-child-label"
               name="selectedChild"
-              label="Name"
-              select
-              value={formData.selectedChild.id}
-              onChange={handleChange}
+              multiple
+              value={formData.selectedChild.map(child => child.id) || []}
+              onChange={(e) => {
+                const { value } = e.target;
+                const selectedChildren = value.map(id =>
+                  childrenOptions.find(child => child.id === id)
+                );
+                setFormData({ ...formData, selectedChild: selectedChildren });
+              }}
+              input={<OutlinedInput label="Name" />}
               required
-              fullWidth
-              style={{ marginTop: '1rem' }}
             >
-              { childrenOptions.map((child) => <MenuItem key={child.id} value={child.id}> {child.name} </MenuItem>) }
-            </TextField>
-            <TextField
-              name="date"
-              label="Date"
-              type="date"
-              value={formData.date}
-              onChange={handleChange}
-              required
-              fullWidth
-              style={{ marginTop: '1rem' }}
-            />
-            <TextField
-              name="start"
-              label="Dropoff Time"
-              type="time"
-              value={formData.start}
-              onChange={handleChange}
-              required
-              fullWidth
-              style={{ marginTop: '1rem' }}
-            />
-            <TextField
-              name="end"
-              label="Pickup Time"
-              type="time"
-              value={formData.end}
-              onChange={handleChange}
-              required
-              fullWidth
-              style={{ marginTop: '1rem' }}
-            />
-          </form>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} color="primary">
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
+              {childrenOptions.map((child) => (
+                <MenuItem key={child.id} value={child.id}>
+                  {child.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <TextField
+            name="date"
+            label="Date"
+            type="date"
+            value={formData.date}
+            onChange={handleChange}
+            required
+            fullWidth
+            style={{ marginTop: '1rem' }}
+          />
+          <TextField
+            name="start"
+            label="Dropoff Time"
+            type="time"
+            value={formData.start}
+            onChange={handleChange}
+            required
+            fullWidth
+            style={{ marginTop: '1rem' }}
+          />
+          <TextField
+            name="end"
+            label="Pickup Time"
+            type="time"
+            value={formData.end}
+            onChange={handleChange}
+            required
+            fullWidth
+            style={{ marginTop: '1rem' }}
+          />
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleSubmit} color="primary">
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
