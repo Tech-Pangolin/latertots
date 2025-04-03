9a55ecc3d19a35b5e1e16a7ca703880b466cd3f7
Terra Taylor <terra.taylor@gmail.com>
2025-04-03 18:31:49 -0500

    WIP

--- src/components/Shared/ChildCard.js
+++ src/components/Shared/ChildCard.js
@@ -9,7 +9,7 @@ function ChildCard({ child, onNameClick }) {
   const navigate = useNavigate();
 
   const getAvatarImgName = () => {
-    switch(child.Gender) {
+    switch (child.Gender) {
       case 'male':
         return 'boy_avatar.png';
       case 'female':
@@ -23,39 +23,62 @@ function ChildCard({ child, onNameClick }) {
     // Navigate to the child registration page with the child object in the location state
     navigate(`/addChild/${child.id}`, { state: { child: child } });
   }
-  
+
 
   return (
-    <Card sx={{ maxWidth: 275, mx: '20px' }}>
-      <div style={{ position: 'relative' }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image={process.env.PUBLIC_URL + getAvatarImgName()}
-          alt="Image description"
-          sx={{ objectFit: 'contain' }}
-        />
-        <div style={{ position: 'absolute', top: 0, right: 0 }}>
-          <IconButton aria-label="favorite" onClick={handleEditClick}>
-            <EditIcon />
-          </IconButton>
+    <>
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              ...
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
         </div>
       </div>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div" onClick={ () => onNameClick(child) }>
-          {child.Name}
-        </Typography>
-        {/* <Rating name="read-only" value={3} readOnly /> */}
-        <Typography variant="body2" color="text.secondary">
-          Age: { calculateAge(child.DOB)} years
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" startIcon={<AddIcon />}>
-          Schedule
-        </Button>
-      </CardActions>
-    </Card>
+      <Card sx={{ maxWidth: 275, mx: '20px' }}>
+        <div style={{ position: 'relative' }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={process.env.PUBLIC_URL + getAvatarImgName()}
+            alt="Image description"
+            sx={{ objectFit: 'contain' }}
+          />
+          <div style={{ position: 'absolute', top: 0, right: 0 }}>
+            <IconButton aria-label="favorite" onClick={handleEditClick}>
+              <EditIcon />
+            </IconButton>
+          </div>
+        </div>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" onClick={() => onNameClick(child)}>
+            {child.Name}
+          </Typography>
+          {/* <Rating name="read-only" value={3} readOnly /> */}
+          <Typography variant="body2" color="text.secondary">
+            Age: {calculateAge(child.DOB)} years
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" startIcon={<AddIcon />}>
+            Schedule
+          </Button>
+        </CardActions>
+      </Card>
+    </>
   );
 }
 
