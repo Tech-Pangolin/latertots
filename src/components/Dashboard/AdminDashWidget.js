2e3f2178dfe98c166298f5d91835cdf94add7fcd
Grey Elerson <grey.elerson@fauna.com>
2025-05-28 14:33:07 -0600

    feat(widgets): organize the static widgets into AdminDashWidget component

---
+++ src/components/Dashboard/AdminDashWidget.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function AdminDashWidget( props ) {
+  const { title, totalValue, dateRange, percentageChange } = props;
+
+  const formatPercentageChange = (change) => {
+    return change >= 0 ? `${change}% increase` : `${change * -1}% decrease`;
+  };
+
+  return (
+    <div className="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0">
+      <div className="card">
+        <h5 className="card-header">{title}</h5>
+        <div className="card-body">
+          <h5 className="card-title">{totalValue}</h5>
+          <p className="card-text">{dateRange}</p>
+          <p className={`card-text ${percentageChange >= 0 ? 'text-success' : 'text-danger'}`}>
+            {formatPercentageChange(percentageChange)} since last month
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/Dashboard/DashHome.js
+++ src/components/Dashboard/DashHome.js
@@ -2,6 +2,7 @@ import React, { useRef, useState, useEffect, useCallback } from 'react';
 import DashboardCalendar from '../Shared/DashboardCalendar';
 import AdminCalendarInteractionColumn from './AdminCalendarInteractionColumn';
 import { AdminPanelContextProvider } from './AdminPanelContext';
+import AdminDashWidget from './AdminDashWidget';
 
 export default function DashHome() {
   // For measuring the calendar's height
@@ -35,65 +36,39 @@ export default function DashHome() {
 
   return (
     <>
-      <h1 className="h2">Dashboard</h1>
-      <p>
-        This is the homepage of a simple admin interface which is part of a tutorial
-        written on Themesberg
-      </p>
+      <h1 className="h2">Admin Dashboard</h1>
 
       {/* Top-row statistic cards */}
       <div className="row my-4">
-        <div className="col-12 col-md-6 col-lg-3 mb-4 mb-lg-0">
-          <div className="card">
-            <h5 className="card-header">Customers</h5>
-            <div className="card-body">
-              <h5 className="card-title">345k</h5>
-              <p className="card-text">Feb 1 – Apr 1, United States</p>
-              <p className="card-text text-success">
-                18.2% increase since last month
-              </p>
-            </div>
-          </div>
-        </div>
 
-        <div className="col-12 col-md-6 mb-4 mb-lg-0 col-lg-3">
-          <div className="card">
-            <h5 className="card-header">Revenue</h5>
-            <div className="card-body">
-              <h5 className="card-title">$2.4k</h5>
-              <p className="card-text">Feb 1 – Apr 1, United States</p>
-              <p className="card-text text-success">
-                4.6% increase since last month
-              </p>
-            </div>
-          </div>
-        </div>
+        <AdminDashWidget 
+          title="Customers"
+          totalValue="345k"
+          dateRange="Feb 1 - Apr 1, United States"
+          percentageChange="18.2"  
+        />
+        
+        <AdminDashWidget
+          title="Revenue"
+          totalValue="$2.4k"
+          dateRange="Feb 1 - Apr 1, United States"
+          percentageChange="4.6"
+        />
 
-        <div className="col-12 col-md-6 mb-4 mb-lg-0 col-lg-3">
-          <div className="card">
-            <h5 className="card-header">Purchases</h5>
-            <div className="card-body">
-              <h5 className="card-title">43</h5>
-              <p className="card-text">Feb 1 – Apr 1, United States</p>
-              <p className="card-text text-danger">
-                2.6% decrease since last month
-              </p>
-            </div>
-          </div>
-        </div>
+        <AdminDashWidget
+          title="Purchases"
+          totalValue="43"
+          dateRange="Feb 1 - Apr 1, United States"
+          percentageChange="-2.6"
+        />
 
-        <div className="col-12 col-md-6 mb-4 mb-lg-0 col-lg-3">
-          <div className="card">
-            <h5 className="card-header">Traffic</h5>
-            <div className="card-body">
-              <h5 className="card-title">64k</h5>
-              <p className="card-text">Feb 1 – Apr 1, United States</p>
-              <p className="card-text text-success">
-                2.5% increase since last month
-              </p>
-            </div>
-          </div>
-        </div>
+        <AdminDashWidget
+          title="Traffic"
+          totalValue="64k"
+          dateRange="Feb 1 - Apr 1, United States"
+          percentageChange="2.5"
+        />
+
       </div>
 
       {/* The notifications feed on the right has its height dynamically set to match the calendar on the left using the calCardRef */}
