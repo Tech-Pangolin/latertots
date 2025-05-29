457866ec92207f48dc60f55c22545018f1527872
Grey Elerson <grey.elerson@fauna.com>
2025-05-29 15:23:26 -0600

    feat(rquery): implement react query in AdminChildren

--- src/components/Dashboard/AdminChildren.js
+++ src/components/Dashboard/AdminChildren.js
@@ -1,29 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../AuthProvider";
-import { logger } from "../../Helpers/logger";
+import { useQuery } from "@tanstack/react-query";
 
 const AdminChildren = () => {
-  const [children, setChildren] = useState(null);
   const { dbService } = useAuth();
 
-  useEffect(() => {
-    if (!dbService) return;
-    const getChildren = async () => dbService.fetchAllChildren()
-      .then((data) => {
-        logger.info("Fetched all children: ", data);
-        setChildren(data);
-      }).catch((error) => {
-        console.error(error);
-      });
+  const {
+    data: children = [],
+    isLoading,
+    isError
+  } = useQuery({
+    queryKey: ['adminAllChildren'],
+    queryFn: dbService.fetchAllChildren
+  })
 
-    getChildren();
-  }, [dbService]);
 
   const formatTableRow = (child) => {
     return (
       <tr key={child.id}>
         <th scope="row">{child.Name || '--'}</th>
-        <td>{child.DOB || '--'}</td>
+        <td>{new Date(child.DOB.seconds).toLocaleDateString() || '--'}</td>
         <td>{child.Gender || '--'}</td>
         <td>{child.Medications || '--'}</td>
         <td>{child.Allergies || '--'}</td>
@@ -40,21 +36,24 @@ const AdminChildren = () => {
             <h5 className="card-header">All Children</h5>
             <div className="card-body">
               <div className="table-responsive">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th scope="col">Name</th>
-                      <th scope="col">DOB</th>
-                      <th scope="col">Gender</th>
-                      <th scope="col">Meds</th>
-                      <th scope="col">Allergies</th>
-                      <th scope="col">Notes</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {children && children.map((child) => formatTableRow(child))}
-                  </tbody>
-                </table>
+                {isLoading && <p>Loading...</p>}
+                {!isLoading && !isError && (
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">DOB</th>
+                        <th scope="col">Gender</th>
+                        <th scope="col">Meds</th>
+                        <th scope="col">Allergies</th>
+                        <th scope="col">Notes</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {children && children.map((child) => formatTableRow(child))}
+                    </tbody>
+                  </table>
+                )}
               </div>
               {/* <a href="#" className="btn btn-block btn-light">View all</a> */}
             </div>
