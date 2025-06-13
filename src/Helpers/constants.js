716bf321191f1981d1dd2037f90c9367296f2132
Grey Elerson <grey.elerson@fauna.com>
2025-06-13 13:38:23 -0600

    refactor: expand constants

--- src/Helpers/constants.js
+++ src/Helpers/constants.js
@@ -8,3 +8,10 @@ export const COLLECTIONS = {
 }
 
 export const MIN_RESERVATION_DURATION_MS = 2 * 60 * 60 * 1000; // 2 hours in milliseconds
+
+export const BUSINESS_HRS = {
+  daysOfWeek: [1, 2, 3, 4, 5], // Monday - Friday
+  startTime: '07:00',
+  endTime: '19:00',
+  overlap: false
+};
