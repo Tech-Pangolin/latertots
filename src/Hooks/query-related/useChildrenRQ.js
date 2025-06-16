cee13c70e9e8ee0d79fc3d7f25e81419037e00e4
Grey Elerson <grey.elerson@fauna.com>
2025-06-16 17:08:51 -0600

    fix(hooks): add guard clauses for when user has no children or contacts

--- src/Hooks/query-related/useChildrenRQ.js
+++ src/Hooks/query-related/useChildrenRQ.js
@@ -1,46 +1,51 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "../../components/AuthProvider";
-import { collection, documentId, query, where } from "firebase/firestore";
+import { collection, documentId, DocumentReference, query, where } from "firebase/firestore";
 import { db } from "../../config/firestore";
 import { useEffect, useMemo } from "react";
 import { COLLECTIONS } from "../../Helpers/constants";
-
+import { logger } from "../../Helpers/logger";
 export function useChildrenRQ() {
   const { dbService, currentUser } = useAuth();
   const queryClient = useQueryClient();
   const isAdmin = currentUser?.Role === 'admin';
   const queryKey = isAdmin ? ['adminAllChildren'] : ['fetchChildren', currentUser.Email];
 
+
   // First, build the query
-  const allChildren = useMemo(() => query(
+  const allChildrenQuery = useMemo(() => query(
     collection(db, COLLECTIONS.CHILDREN),
     where("archived", "==", false)
   ), [])
 
-  const myChildren = useMemo(() => {
+  const myChildrenQuery = useMemo(() => {
+    if (currentUser.Children.length === 0) return null; // If the user has no children, return null to avoid query errors
     return query(
       collection(db, COLLECTIONS.CHILDREN),
       where(documentId(), "in", currentUser.Children), // This only allows up to 30 children before the "in" operator fails
-      where("archived", "==", false) 
+      where("archived", "==", false)
     )
-  },[JSON.stringify(currentUser.Children)]);
+  }, [JSON.stringify(currentUser.Children)]);
 
   // Second, get the initial fetch of data
   const queryResult = useQuery({
     queryKey,
-    queryFn: () => dbService.fetchDocs(isAdmin? allChildren : myChildren, isAdmin),
+    queryFn: () => dbService.fetchDocs(isAdmin ? allChildrenQuery : myChildrenQuery),
     onError: (error) => {
-      console.error("Error fetching /Children data:", error);
+      logger.error("Error fetching /Children data:", error);
     },
+    enabled: currentUser.Children.length > 0 || isAdmin, // Only run if the user has children or is an admin
+    initialData: []
   })
 
   // Third, set up a real-time listener for changes
   useEffect(() => {
-    const unsubscribe = dbService.subscribeDocs(isAdmin ? allChildren : myChildren, fresh => {
+    if (!myChildrenQuery && !isAdmin) return; // If the user has no children and is not admin, do not set up a listener
+    const unsubscribe = dbService.subscribeDocs(isAdmin ? allChildrenQuery : myChildrenQuery, fresh => {
       queryClient.setQueryData(queryKey, fresh);
-    }, isAdmin)
+    })
     return () => unsubscribe();
-  }, [dbService, queryClient, allChildren, myChildren, queryKey]);
+  }, [dbService, queryClient, allChildrenQuery, myChildrenQuery, queryKey]);
 
   return queryResult;
 }
--- src/Hooks/query-related/useContactsRQ.js
+++ src/Hooks/query-related/useContactsRQ.js
@@ -11,31 +11,36 @@ export function useContactsRQ() {
   const queryClient = useQueryClient();
   const queryKey = isAdmin ? ['adminAllContacts'] : ['fetchContacts', currentUser.Email];
 
-  const allContacts = useMemo( () => query(
+  const allContacts = useMemo(() => query(
     collection(db, COLLECTIONS.CONTACTS),
     where("archived", "==", false),     // TODO: Make sure dummy data has this field set correctly
   ), [])
 
   const myContacts = useMemo(() => {
+    if (currentUser.Contacts.length === 0) return null; // If the user has no contacts, return null to avoid query errors
     return query(
-          collection(db, COLLECTIONS.CONTACTS),
-          where(documentId(), "in", currentUser.Contacts), // This only allows up to 30 contacts before the "in" operator fails
-          where("archived", "==", false) 
-        )
+      collection(db, COLLECTIONS.CONTACTS),
+      where(documentId(), "in", currentUser.Contacts), // This only allows up to 30 contacts before the "in" operator fails
+      where("archived", "==", false)
+    )
   }, [])
 
   const queryResult = useQuery({
     queryKey,
-    queryFn: () => dbService.fetchDocs(isAdmin ? allContacts : myContacts, isAdmin ? true : false),
+    queryFn: () => dbService.fetchDocs(isAdmin ? allContacts : myContacts),
     onError: (error) => console.error("Error fetching /Contacts data:", error),
+    enabled: currentUser.Contacts.length > 0 || isAdmin, 
+    initialData: []
   })
 
   useEffect(() => {
+    if (!myContacts && !isAdmin) return; // If the user has no contacts and is not admin, do not set up a listener
+    
     const unsub = dbService.subscribeDocs(isAdmin ? allContacts : myContacts, fresh => {
       queryClient.setQueryData(queryKey, fresh);
-    }, isAdmin ? true : false);
+    });
     return () => unsub();
-  },[allContacts, dbService, queryKey, queryClient])
+  }, [allContacts, dbService, queryKey, queryClient])
 
   return queryResult;
 }
