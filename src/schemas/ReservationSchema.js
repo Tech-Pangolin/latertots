c1bc8d177f3221af25d9d880106fa8faac29bde7
Grey Elerson <grey.elerson@fauna.com>
2025-06-23 15:00:37 -0600

    feat(schema): define schema for Reservations collection

---
+++ src/schemas/ReservationSchema.js
@@ -0,0 +1,25 @@
+import Joi from 'joi';
+import { RESERVATION_STATUS } from '../Helpers/constants';
+import { DocumentReference } from 'firebase/firestore';
+
+const ReservationSchema = Joi.object({
+  // Administrative fields
+  archived: Joi.boolean().required(),
+  billingLocked: Joi.boolean().required(),
+  allDay: Joi.boolean().required(),
+  overrideTotalCents: Joi.number().integer().min(0).optional(),       // Optional field for overriding total billing amount
+  
+  // Reservation fields
+  start: Joi.date().required(),
+  end: Joi.date().required(),
+  title: Joi.string().required(),
+  extendedProps: Joi.object({
+    status: Joi.string()
+                .valid(...Object.values(RESERVATION_STATUS))
+                .required(),
+    childId: Joi.string().required(),
+  }),
+  userId: Joi.string().required(),
+  Child: Joi.object().instance(DocumentReference).required(),
+  User: Joi.object().instance(DocumentReference).required(),
+})
